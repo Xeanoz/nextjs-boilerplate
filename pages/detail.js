7a1a469f590aa0b8155284a2b8b71fd1081c4c0b
Nopdanai Leelakanjanakul <tenzndn@gmail.com>
2023-05-22 07:46:40 +0000

    Update detail.js and upload.js

--- pages/detail.js
+++ pages/detail.js
@@ -57,12 +57,8 @@ function Deatiljs() {
             </div>
           </div>
           <div className="lyric-box-box">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat laborum.
+            Let's describe something important for your song, like idea ,
+            concept or taking some note about your project here!
           </div>
         </div>
       </div>
--- pages/upload.js
+++ pages/upload.js
@@ -11,21 +11,22 @@ function Uploadjs() {
         <Link className="button1 ml-4 w-12 cancel " href="/">
           Cancel
         </Link>
-        <div className="filename">final_draft_imy.mp4</div>
+        <div className="filename">filename_filename.mp4</div>
         <div className="button1 flex justify-end save w-12 mr-4">Save</div>
       </div>
-      <div className="flex justify-center button1 mt-20">
+      <div className="flex justify-center button3 mt-20">
         <img src="/img/InsertPicture.svg" />
       </div>
       <div className="bottom-upload mx-4">
         <div className="title-box mt-8">
           <div className="title">Title</div>
-          <div className="title-input mt-1">I missed you</div>
+          <div className="title-input mt-1">Filename filename</div>
         </div>
         <div className="mt-5">
           <div className="title">Note</div>
           <div className="note-input mt-1 leading-6 mb-8">
-            Descrpition about this song or note something here!
+            Let's describe something important for your song, like idea ,
+            concept or taking some note about your project here!
           </div>
         </div>
       </div>
